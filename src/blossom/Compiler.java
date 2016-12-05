4ce4cc8ae178b6be4b0f7a3cb761de54d761dba7
Huw Taylor <huw_taylor@hotmail.co.uk>
2016-12-05 23:08:39 +0000

    basic gitignore

---
+++ src/blossom/Compiler.java
@@ -0,0 +1,17 @@
+package blossom;
+
+public class Compiler {
+    
+    private static void printUsage() {
+        System.out.println("");
+    }
+
+    public static void main(String... args) {
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+        
+    }
+
+}
