2a3230f6f3d0490ee675d9e9881fbc9c20ecd28f
Huw Taylor <huw_taylor@hotmail.co.uk>
2016-12-07 22:06:05 +0000

    added initial test

--- src/blossom/compiler/Compiler.java
+++ src/blossom/compiler/Compiler.java
@@ -1,4 +1,6 @@
-package blossom;
+package blossom.compiler;
+
+import blossom.lang.Graph;
 
 public class Compiler {
     
@@ -7,11 +9,18 @@ public class Compiler {
     }
 
     public static void main(String... args) {
+    	test();
         if (args.length == 0) {
             printUsage();
             return;
         }
         
+    }
+    
+    public static void test() {
+    	String graphText = "[ 1 (#red), 2 (4) | 1->2 ]";
+    	Graph g = new GraphParser(graphText, false).parse();
+    	System.out.println(g);
     }
 
 }
