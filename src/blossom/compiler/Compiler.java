600700ef85de2473026b154e4b417b81e34cd81c
IMP1 <IMP1@mail.com>
2016-12-08 09:57:41 +0000

    added example of handling pipeline arguments

--- src/blossom/compiler/Compiler.java
+++ src/blossom/compiler/Compiler.java
@@ -10,10 +10,23 @@ public class Compiler {
 
     public static void main(String... args) {
     	test();
-        if (args.length == 0) {
-            printUsage();
-            return;
+
+        // Allow for Piped-in Arguments
+        try (InputStreamReader streamReader = new InputStreamReader(System.in)) {
+            StringBuilder pipelineArgs = new StringBuilder();
+            char[] buffer = new char [256];
+            int amountRead = 0;
+            while (streamReader.ready() && amountRead > -1) {
+                int amountRead = streamReader.read(buffer, 0, buffer.length);
+                if (amountRead > -1) {
+                    pipelineArgs.append(buffer, 0, amountRead);
+                }
+            }
+            System.out.printf("Read '%s' from SYSIN.\n", pipelineArgs.toString());
         }
+
+        // TODO: Handle both args and pipelineArgs
+        // ...
         
     }
     
