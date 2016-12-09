beeafee1855231cb682fa13706cabba9f7db3462
IMP1 <IMP1@mail.com>
2016-12-09 17:08:01 +0000

    added programme

---
+++ src/blossom/lang/Programme.java
@@ -0,0 +1,32 @@
+package blossom.lang;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Programme {
+
+    private HashMap<String, Rule> rules;
+    private HashMap<String, Procedure> procedures;
+    private ArrayList<Istruction> instructions;
+    
+    public Programme() {
+
+    }
+
+    public void addRule(String name, Rule rule) {
+        rules.put(name, rule);
+    }
+
+    public void addProcedure(String name, Procedure procedure) {
+        procedures.put(name, procedure);
+    }
+
+    public void addInstruction(Instruction newInstruction) {
+        instructions.add(newInstruction);
+    }
+
+    public void run() {
+        // TODO
+    }
+    
+}
