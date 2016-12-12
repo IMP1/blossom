022decfb8bb25710951b70d4cac7318fbfa3f811
IMP1 <IMP1@mail.com>
2016-12-12 11:49:14 +0000

    added a matcher for applying rules

---
+++ src/blossom/lang/Matcher.java
@@ -0,0 +1,17 @@
+package blossom.lang;
+
+public class Matcher { 
+
+    private Graph hostGraph;
+    private Graph applicationGraph;
+
+    public Matcher(Graph hostGraph, Graph applicationGraph) {
+        this.hostGraph = hostGraph;
+        this.applicationGraph = applicationGraph;
+    }
+
+    public boolean find() {
+
+    }
+
+}
