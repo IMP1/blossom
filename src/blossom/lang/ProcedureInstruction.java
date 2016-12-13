53b002e326978bf85c7357208aa1c7382f99b9f9
IMP1 <IMP1@mail.com>
2016-12-13 10:32:38 +0000

    added simplest concrete instruction - single proc call

---
+++ src/blossom/lang/ProcedureInstruction.java
@@ -0,0 +1,13 @@
+package blossom.lang;
+
+public class ProcedureInstruction extends Instruction {
+
+    public ProcedureInstruction() {
+        
+    }
+
+    public Graph execute(Graph g) {
+        return null;
+    }
+    
+}
