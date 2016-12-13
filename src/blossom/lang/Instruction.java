dc77795d4e8c47e0627098d2483350ee5b866b2e
IMP1 <IMP1@mail.com>
2016-12-13 12:04:38 +0000

    added multiplicity

--- src/blossom/lang/Instruction.java
+++ src/blossom/lang/Instruction.java
@@ -1,19 +1,25 @@
 package blossom.lang;
 
+import java.util.ArrayList;
+
 public abstract class Instruction {
 
     public static final String IF_KEYWORD   = "if";
     public static final String WITH_KEYWORD = "with";
     public static final String TRY_KEYWORD  = "try";
 
-    
+    public enum Multiplicity {
+        ONCE,
+        OPTIONAL,
+        WHILE_POSSIBLE
+    }
 
-    public final 
+    public final Multiplicity multiplicity;
 
     public Instruction() {
 
     }
 
-    public abstract Graph execute(Graph g);
+    public abstract Graph execute(final Graph g);
     
 }
