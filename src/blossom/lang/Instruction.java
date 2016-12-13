c9f19e682859e1b14167b42f80c7875dcf284f37
IMP1 <IMP1@mail.com>
2016-12-13 10:32:08 +0000

    added some keyword constants and made it abstract

--- src/blossom/lang/Instruction.java
+++ src/blossom/lang/Instruction.java
@@ -1,13 +1,19 @@
 package blossom.lang;
 
-public class Instruction {
+public abstract class Instruction {
+
+    public static final String IF_KEYWORD   = "if";
+    public static final String WITH_KEYWORD = "with";
+    public static final String TRY_KEYWORD  = "try";
+
+    
+
+    public final 
 
     public Instruction() {
 
     }
 
-    public Graph execute(Graph g) {
-    	return null;
-    }
+    public abstract Graph execute(Graph g);
     
 }
