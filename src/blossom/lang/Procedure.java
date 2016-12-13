a6429d1597da5aa88ddd671ff08da1c120f49374
IMP1 <IMP1@mail.com>
2016-12-13 15:29:41 +0000

    added application of procs

--- src/blossom/lang/Procedure.java
+++ src/blossom/lang/Procedure.java
@@ -7,11 +7,14 @@ public class Procedure {
     public static final String DEFINITION_KEYWORD = "proc";
 	public static final String END_KEYWORD = "end";
 
-    private ArrayList<Instruction> instructions;
+    private InstructionSequence instructions;
     
     public Procedure() {
 
     }
-    
-    
+
+    public Graph apply(Graph g) {
+        return instructions.apply(g);
+    }
+
 }
