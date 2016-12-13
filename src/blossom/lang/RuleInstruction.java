3e221af4550ff555e086ff99e1257c0d7c173443
IMP1 <IMP1@mail.com>
2016-12-13 12:06:14 +0000

    applies a single rule according to its multiplicity

--- src/blossom/lang/RuleInstruction.java
+++ src/blossom/lang/RuleInstruction.java
@@ -2,12 +2,28 @@ package blossom.lang;
 
 public class RuleInstruction extends Instruction {
 
-    public RuleInstruction() {
-        
+    public final Rule rule;
+
+    public RuleInstruction(Rule rule) {
+        this.rule = rule;
     }
 
-    public Graph execute(Graph g) {
-        return null;
+    public Graph execute(final Graph g) {
+        switch (multiplicity) {
+        case OPTIONAL:
+            Graph result = rule.apply(g);
+            return result == Graph.INVALID ? g : result;
+        case ONCE:
+            return rule.apply(g);
+        case WHILE_POSSIBLE:
+            Graph result = g;
+            Graph currentGraph = rule.apply(g);
+            while (currentGraph != Graph.INVALID) {
+                result = currentGraph;
+                currentGraph = rule.apply(currentGraph);
+            }
+            return result;
+        }
     }
     
 }
