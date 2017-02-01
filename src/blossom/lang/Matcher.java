aa0bc12e4c67715640b441cfbdf07f51947080a3
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-02-01 21:17:46 +0000

    tried something new with the matching

--- src/blossom/lang/Matcher.java
+++ src/blossom/lang/Matcher.java
@@ -7,42 +7,49 @@ import blossom.lang.Rule.Variable;
 
 public class Matcher {
 
-    private Graph                     hostGraph;
-    private Rule                      rule;
-    private Graph                     applicationGraph;
-    private ArrayList<Variable>       variables;
-
-    private int skip = 0;
-    
-    private HashMap<Integer, Integer> nodeMappings;
-
-    public Matcher(Graph hostGraph, Rule rule) {
-        this.hostGraph        = hostGraph;
-        this.rule             = rule;
-        this.applicationGraph = rule.initialGraph;
-        this.variables        = rule.variables;
-        this.nodeMappings     = new HashMap<Integer, Integer>();
-    }
-
-    public boolean find() {
-        return nextMatch() != null;
+    public static HashMap<Integer, Integer> getMatch(Graph hostGraph, Rule rule) {
+    	return getMatch(new HashMap<Integer, Integer>(), hostGraph, rule, 0);
     }
     
-    public HashMap<Integer, Integer> nextMatch() {
-    	HashMap<Integer, Integer> match = new HashMap<Integer, Integer>();
-    	for (int i = 0; i < rule.initialGraph.nodes().length; i ++) {
-    		
-    	}
-    	return null; // TODO: do.
+    private static HashMap<Integer, Integer> getMatch(HashMap<Integer, Integer> substitutions, 
+                                                      Graph hostGraph, 
+                                                      Rule rule, 
+                                                      int depth) {
+        for (int i = 0; i < rule.initialGraph.nodes().length; i ++) {
+            if (substitutions.containsKey(i)) continue;
+            
+            Node ruleNode = rule.initialGraph.nodes()[i];
+            
+            for (int j = 0; j < hostGraph.nodes().length; j ++) {
+                Node graphNode = hostGraph.nodes()[i];
+                
+                if (nodesMatch(graphNode, ruleNode)) {
+                    HashMap<Integer, Integer> subSubstitutions = (HashMap<Integer, Integer>)substitutions.clone();
+                    subSubstitutions.put(i, j);
+                    
+                    subSubstitutions = getMatch(substitutions, hostGraph, rule, depth + 1);
+                    if (subSubstitutions.size() == rule.initialGraph.nodes().length) {
+                        boolean validMatch = testMatch(subSubstitutions);
+                        if (validMatch) return subSubstitutions;
+                    }
+                            
+                }
+            }
+        }
+        return substitutions;
+    }
+    
+    private static boolean testMatch(HashMap<Integer, Integer> substitutions) {
+        return true;
     }
 
-    private boolean nodesMatch(Node hostGraphNode, Node applicationGraphNode) {
-    	if (Functions.in(hostGraph, hostGraphNode.id) 
-		 != Functions.in(applicationGraph, applicationGraphNode.id)) return false;
-    	if (Functions.out(hostGraph, hostGraphNode.id) 
-		 != Functions.out(applicationGraph, applicationGraphNode.id)) return false;
-    	if (Functions.in(hostGraph, hostGraphNode.id) != Functions.in(applicationGraph, applicationGraphNode.id)) return false;
-    	return false; // TODO: do
+    private static boolean nodesMatch(Node hostGraphNode, Node ruleGraphNode) {
+        if (ruleGraphNode.label() != null) {
+            for (String mark : ruleGraphNode.label().marks) {
+                if (!hostGraphNode.label().hasMark(mark)) return false;
+            }
+        }
+        return true;
     }
 
 }
