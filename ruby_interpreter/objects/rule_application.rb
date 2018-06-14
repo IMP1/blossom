0d41254e131e56c847a5d04967104acca009db5d
Huw Taylor <huw_taylor@hotmail.co.uk>
2018-06-14 19:09:50 +0100

    better structured possible mappings - added TODOs

--- ruby_interpreter/objects/rule_application.rb
+++ ruby_interpreter/objects/rule_application.rb
@@ -15,30 +15,6 @@ class RuleApplication
         graph = @graph
         @log.trace("Attempting to apply a rule.")
 
-        possible_matches = {}
-
-        rule.match_graph.nodes.each do |rule_node|
-            possible_matches[rule_node] = graph.nodes.select { |graph_node| 
-                nodes_match?(rule_node, graph_node, rule.match_graph.edges, graph.edges) 
-            }
-        end
-
-        # TODO: Create sets of possible permutations, rather than just possiblities for each node.
-        #       This, I think, is the next step /after/ working out possibilities for each node.
-        #       Or maybe just create all permutations (n*m) and check which are possible?
-        #       This is probably the only challege left to do, and I reckon it's going to be the 
-        #       most intensive part of the interpreter.
-
-        # IDEA: Maybe go through the first node in the rule graph, and get all its possible matches.
-        #       Then go through all of the those possible matches, and try to add the matches of the 
-        #       second node (and so on). So you're left with an array of hashmaps.
-        #       After first pass:
-        #       possible_matches = [ {1=>1}, {1=>2} ]
-        #       After second pass (using first pass matches):
-        #       possible_matches = [ {1=>1, 2=>2}, {1=>2, 2=>3} ]
-        #       This stops the current problem of finding the possible matches:
-        #       {1=>[1, 2], 2=>[2, 3]} without accounting for the combinations of these matches.
-
         @log.trace("Rule Match Graph:")
         @log.trace(rule.match_graph.to_s)
         @log.trace("Rule Result Graph:")
@@ -50,43 +26,92 @@ class RuleApplication
         removed_rule_nodes = rule.match_graph.nodes.select { |node| 
             !rule.result_graph.nodes.any? { |n| node.id == n.id }
         }
+        @log.trace("#{added_rule_nodes.size} nodes to add.")
+        @log.trace("#{removed_rule_nodes.size} nodes to remove.")
 
-        adjacent_edge_count = {}
-        graph.nodes.each do |n|
-            adjacent_edge_count[n] = graph.edges.count { |e| e.source_id == n.id || e.target_id == n.id }
-        end
-        rule.match_graph.nodes.each do |n|
-            adjacent_edge_count[n] = graph.edges.count { |e| e.source_id == n.id || e.target_id == n.id }
-        end
+        possible_matches = {}
 
-        # TODO: Any removed nodes must not be incident to any edges not in the rule.
-        #       (the host graph's node must not have a higher incident value than the initial graph's node).
-        #       Otherwise it will not satisfy the DANGLING CONDITION.
-
-
-
-        removed_rule_nodes.each do |rule_node|
-            possible_matches[rule_node] = possible_matches[rule_node].select { |graph_node|
-                p adjacent_edge_count[graph_node];
-                p adjacent_edge_count[rule_node];
-                adjacent_edge_count[graph_node] <= adjacent_edge_count[rule_node]
+        rule.match_graph.nodes.each do |rule_node|
+            possible_matches[rule_node] = graph.nodes.select { |graph_node| 
+                nodes_match?(rule_node, graph_node, rule.match_graph.edges, graph.edges) 
             }
         end
 
-        @log.trace("Adding #{added_rule_nodes.size} nodes.")
-        @log.trace("Removing #{removed_rule_nodes.size} nodes.")
+        possible_matches = [{}]
 
-        puts "Rule Node | Graph Node Matches"
-        possible_matches.each do |k, v|
-            print " " + k.id.to_s.ljust(9) + "  "
-            print v.empty? ? "--" : v[0].id
-            print "\n"
-            v[1..-1].each do |m|
-                print " " * 12
-                print m.id
-                print "\n"
+        rule.match_graph.nodes.each do |rule_node|
+            node_matches = graph.nodes.select do |graph_node| 
+                nodes_match?(rule_node, graph_node, rule.match_graph.edges, graph.edges) 
+            end
+            possible_matches = possible_matches.map { |existing_match|
+                node_matches.map { |matched_node| 
+                    new_match = existing_match.clone
+                    new_match[rule_node] = matched_node
+                    new_match
+                }.flatten
+            }.flatten
+        end
+
+        @log.trace("Initial possible mappings:")
+        @log.trace(possible_matches.map { |pm| 
+            pm.map { |k, v| 
+                "#{k.id} => #{v.id}" 
+            }.join(", ") 
+        }.join("\n"))
+
+        # Any removed nodes must not be incident to any edges not in the rule.
+        # (the host graph's node must not have a higher incident value than the initial graph's node).
+        # Otherwise it will not satisfy the DANGLING CONDITION.
+
+        adjacent_graph_edge_count = {}
+        graph.nodes.each do |n|
+            adjacent_graph_edge_count[n] = graph.edges.count { |e| e.source_id == n.id || e.target_id == n.id }
+        end
+        adjacent_rule_edge_count = {}
+        rule.match_graph.nodes.each do |n|
+            adjacent_rule_edge_count[n] = graph.edges.count { |e| e.source_id == n.id || e.target_id == n.id }
+        end
+
+        removed_rule_nodes.each do |removed_rule_node|
+            possible_matches = possible_matches.reject do |mapping| 
+                mapping.has_key?(removed_rule_node) &&
+                adjacent_edge_count[mapping[removed_rule_node]] <= adjacent_edge_count[rule_node]
             end
         end
+
+        @log.trace("Removed mappings that create dangling edges.")
+        @log.trace("Remaining possible mappings:")
+        @log.trace(possible_matches.map { |pm| 
+            pm.map { |k, v| 
+                "#{k.id} => #{v.id}" 
+            }.join(", ") 
+        }.join("\n"))
+
+        # TODO: decide on whether different rule match_graph nodes can be mapped to the same graph node.
+        #       rule r [ 1, 2 ] => [ 1, 2 | 1->2 ];
+        #       applying r on [ 1(3), 2(1) ]
+        #       could yield any of these:
+        #       [ 1(3), 2(1) | 1->2 ]
+        #       [ 1(3), 2(1) | 2->1 ]
+        #       [ 1(3), 2(1) | 1->1 ]    \_ Should these last two be valid? If so, should there be a way
+        #       [ 1(3), 2(1) | 2->2 ]    /  of specifying that they must be different (in the where condition maybe?)
+
+        # TODO: handle matching edges between possible node mappings.
+
+        puts "\nPossible Matches:"
+        possible_matches.each do |pm|
+            pm.each do |k, v|
+                print "#{k.id} => #{v.id}"
+                print ", "
+            end
+            print "\n"
+        end
+
+        # TODO: check rule condition with possible mappings
+
+        # TODO: actually apply mapping (add, remove, and update nodes as necessary.)
+
+        # TODO: execute addendum
 
         return Graph::INVALID
     end
