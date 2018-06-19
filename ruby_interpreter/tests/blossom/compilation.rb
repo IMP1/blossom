ae11b4895e511a08ce5c1f83d0213ac665806494
IMP1 <huw_taylor@hotmail.co.uk>
2018-06-19 16:14:57 +0100

    added tests for more end-to-end testing (and of parsing/interpreteing)

---
+++ ruby_interpreter/tests/blossom/compilation.rb
@@ -0,0 +1,44 @@
+require_relative '../test'
+
+require_relative '../../main'
+
+$verbose = true
+
+#---------#
+# Arrange #
+#---------#
+
+# dangling_condition
+
+PROG_TEXT = <<~HEREDOC
+rule foo    [ 1, 2 | 1->2 ]
+         => [ 1 ]
+end
+foo
+HEREDOC
+
+GRAPH_TEXT = "[1, 2, 3 | 1->2, 2->3 ]"
+
+#-----#
+# Act #
+#-----#
+test_run = Test.run {
+
+    # ARGV = ["tmp.blsm", "\"#{GRAPH_TEXT}\""]
+    # require_relative 
+
+    Runner.run(PROG_TEXT, GRAPH_TEXT, "test", "test")
+
+}
+
+test_run.ensure do |result|
+    p result
+
+    result_graph = result.value
+
+    assert(result_graph.nodes.size == 2, "Result graph should have two nodes.")
+    assert(result_graph.edges.size == 1, "Result graph should have one edge.")
+
+    assert(result_graph.nodes.count {|n| !n.label.nil? && !n.label.value.nil? && n.label.value.value == 0 } > 0)
+
+end
