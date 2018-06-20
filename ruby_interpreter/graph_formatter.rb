c5c8a961ed9f7f91144eeaa35fc7f31de03eb9ea
IMP1 <huw_taylor@hotmail.co.uk>
2018-06-20 17:20:17 +0100

    added better TODOs and removed debug printing

--- ruby_interpreter/graph_formatter.rb
+++ ruby_interpreter/graph_formatter.rb
@@ -13,18 +13,16 @@ class GraphFormatter
 
     def self.blossom_graph(graph)
         puts "Outputting Blossom Graph:"
-        p graph
-        return ""
-        # TODO: do
+        return "<blossom graph goes here>"
+        # TODO: redo printer to be a /blossom/ printer (to output as they're inputted)
         # return graph.join("\n")
     end
 
     def self.dot_graph(graph)
         # https://en.wikipedia.org/wiki/DOT_(graph_description_language)
         puts "Outputting Dot Graph:"
-        p graph
-        return ""
-        # TODO: do
+        return "<dot graph goes here>"
+        # TODO: add a dot printer to output valid dot graphs.
     end
 
 end
