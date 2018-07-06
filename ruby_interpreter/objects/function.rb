364494f14d6eb75debb7fe031d2dde2a970c4159
IMP1 <huw_taylor@hotmail.co.uk>
2018-07-06 16:52:24 +0100

    updated blossom syntax to include builtins and added some comments for builtin functions

--- ruby_interpreter/objects/function.rb
+++ ruby_interpreter/objects/function.rb
@@ -15,16 +15,19 @@ class Function
         return @block.call(evaluator, args)
     end
 
-    def self.node
-        return Function.new("node", Node, [:int]) do |evaluator, args|
-            rule_node_id = args[0]
-            graph_node_id = evaluator.mapping[rule_node_id]
-            graph_node = evaluator.graph.nodes.find { |n| n.id == graph_node_id }
-            graph_node
-        end
-    end
+    # TODO: remove this method:
+    # def self.node
+    #     return Function.new("node", Node, [:int]) do |evaluator, args|
+    #         rule_node_id = args[0]
+    #         graph_node_id = evaluator.mapping[rule_node_id]
+    #         graph_node = evaluator.graph.nodes.find { |n| n.id == graph_node_id }
+    #         graph_node
+    #     end
+    # end
 
     def self.in
+        # Takes an ID of a node and returns how many edges have that node as their target.
+        # (the number of edges coming 'in' to the node).
         return Function.new("in", :int, [:int]) do |evaluator, args|
             rule_node_id = args[0]
             graph_node_id = evaluator.mapping[rule_node_id]
@@ -34,6 +37,8 @@ class Function
     end
 
     def self.out
+        # Takes an ID of a node and returns how many edges have that node as their source.
+        # (the number of edges going 'out' from the node).
         return Function.new("out", :int, [:int]) do |evaluator, args|
             rule_node_id = args[0]
             graph_node_id = evaluator.mapping[rule_node_id]
@@ -43,6 +48,8 @@ class Function
     end
 
     def self.incident
+        # Takes an ID of a node and returns how many edges have that node as their source or their target.
+        # (the number of edges incident to the node).
         return Function.new("incident", :int, [:int]) do |evaluator, args|
             rule_node_id = args[0]
             graph_node_id = evaluator.mapping[rule_node_id]
@@ -52,6 +59,8 @@ class Function
     end
 
     def self.edge
+        # Takes two node IDs and returns how many edges there are that have the first as their source and the second as their target.
+        # (the number of edges from the first node to the second).
         return Function.new("edge", :int, [:int, :int]) do |evaluator, args|
             source_node_id = evaluator.mapping[args[0]]
             target_node_id = evaluator.mapping[args[1]]
@@ -61,6 +70,8 @@ class Function
     end
 
     def self.adj
+        # Takes two node IDs and returns how many edges there are that have either the first or the second as either their source or their target.
+        # (the number of edges between the two nodes).
         return Function.new("adj", :int, [:int, :int]) do |evaluator, args|
             source_node_id = evaluator.mapping[args[0]]
             target_node_id = evaluator.mapping[args[1]]
@@ -71,7 +82,9 @@ class Function
     end
 
     def self.edge?
-        return Function.new("edge?", :int, [:int, :int]) do |evaluator, args|
+        # Takes two node IDs and returns true if there is at least one edge that has the first as its source and the second as its target.
+        # (whether there's an edge from the first node to the second).
+        return Function.new("edge?", :bool, [:int, :int]) do |evaluator, args|
             source_node_id = evaluator.mapping[args[0]]
             target_node_id = evaluator.mapping[args[1]]
             edge_count = evaluator.graph.edges.count { |e| e.source_id == source_node_id && e.target_id == target_node_id }
@@ -80,7 +93,9 @@ class Function
     end
 
     def self.adj?
-        return Function.new("adj?", :int, [:int, :int]) do |evaluator, args|
+        # Takes two node IDs and returns true if there is at least one edge that has the first or the second as its source or its target.
+        # (whether there's an edge between the two nodes).
+        return Function.new("adj?", :bool, [:int, :int]) do |evaluator, args|
             source_node_id = evaluator.mapping[args[0]]
             target_node_id = evaluator.mapping[args[1]]
             edge_count = evaluator.graph.edges.count { |e| (e.source_id == source_node_id && e.target_id == target_node_id) ||
@@ -90,24 +105,28 @@ class Function
     end
 
     def self.str
+        # Takes any value and converts it to a string.
         return Function.new("str", :string, [:any]) do |evaluator, args|
             args[0].to_s
         end
     end
 
     def self.head
+        # Returns the first character of a string
         return Function.new("head", :string, [:string]) do |evaluator, args|
             args[0][0]
         end
     end
 
     def self.tail
+        # Returns all but the first character of a string
         return Function.new("tail", :string, [:string]) do |evaluator, args|
             args[0][1..-1]
         end
     end
 
     def self.len
+        # Returns the length of a string
         return Function.new("len", :int, [:string]) do |evaluator, args|
             args[0].length
         end
