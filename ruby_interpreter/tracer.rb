06c5f3a1b5b73f4eec6024d3c142dbb45b6118a8
IMP1 <huw_taylor@hotmail.co.uk>
2018-07-09 11:48:11 +0100

    CLI handles trace directories, and tracing creates files

--- ruby_interpreter/tracer.rb
+++ ruby_interpreter/tracer.rb
@@ -7,23 +7,24 @@ class Tracer
         tracing_directory ||= DEFAULT_DIRECTORY
         @tracing_directory = tracing_directory
         @saved_graph_count = 0
+
         # TODO: create directory and empty overview file.
     end
 
     def save_graph(graph)
         append("[Graph]")
-        # filename = File.join(@tracing_directory, "graph_#{@saved_graph_count}")
-        # File.open(filename, 'w') do |file|
-        # 
-        # end
+        filename = File.join(@tracing_directory, "graph_#{@saved_graph_count}")
+        File.open(filename, 'w') do |file|
+            file.puts(graph.to_s)
+        end
+        @saved_graph_count += 1
     end
 
     def append(action)
-        # filename = File.join(@@tracing_directory, OVERVIEW_FILENAME)
-        # File.open(filename, 'a') do |file|
-        # 
-        # end
-        puts action
+        filename = File.join(@tracing_directory, OVERVIEW_FILENAME)
+        File.open(filename, 'a') do |file|
+            file.puts(action)
+        end
     end
 
 end
